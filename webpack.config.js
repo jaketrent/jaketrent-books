42260b08abdf71b37a2be6b59570a34c2ca4a42e
jaketrent <trent.jake@gmail.com>
2014-11-16 16:27:59 -0700

    Initial commit
    - Webpack config from Brent
    - Basic server for heroku

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+var DefinePlugin = require('webpack').DefinePlugin
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: './dist',
+    filename: 'main.[hash].js',
+    chunkFilename: 'chunk.[hash].[id].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'jsx-loader?harmony'},
+      {test: /\.scss/, loader:'style-loader!css-loader!less-loader'},
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+    ]
+  },
+  plugins: [
+    new DefinePlugin({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Jake\'s Books'
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+}
