eb560ef35d52df78f1fb3d9c3c234ba993e77941
jaketrent <trent.jake@gmail.com>
2015-04-06 16:09:00 -0600

    extract views init

---
+++ server/config/views.js
@@ -0,0 +1,8 @@
+var path = require('path')
+
+
+exports.init = (app) => {
+  app.set('views', path.resolve(path.join(__dirname, '..', 'views')))
+  app.set('view engine', 'html')
+  app.engine('html', require('ejs').renderFile)
+}
--- server/index.js
+++ server/index.js
@@ -2,23 +2,17 @@ require('babel/register')({
   stage: 0
 })
 require('dotenv').load()
-
 var express = require('express')
-var path = require('path')
-var React = require('react')
-var Router = require('react-router')
 
 var assets = require('./common/assets')
 var routes = require('./config/routes')
-var port = process.env.PORT || 3000
+var views = require('./config/views')
 
+var port = process.env.PORT || 3000
 var app = express()
 
-app.set('views', path.resolve(__dirname + '/views'))
-app.set('view engine', 'html')
-app.engine('html', require('ejs').renderFile)
-
-routes.map(app);
+views.init(app)
+routes.map(app)
 
 assets.upload(function () {
   app.listen(port, function () {
