ef3f6c1b991d8b67a4fdff2d2a5bb1ff572db1a4
jaketrent <trent.jake@gmail.com>
2015-04-10 13:36:31 -0600

    update babel

--- server/config/assets/webpack/config/webpack.config.js
+++ server/config/assets/webpack/config/webpack.config.js
@@ -4,14 +4,13 @@ var path = require('path')
 module.exports = {
   entry: {
     main: [
-      '6to5/polyfill',
-      '6to5/runtime',
+      'babel/polyfill',
       './client/index.js'
     ]
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader?harmony!6to5-loader?experimental&runtime', exclude: /node_modules/ },
+      { test: /\.js$/, loader: 'jsx-loader?harmony!babel-loader?stage=0', exclude: /node_modules/ },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.scss/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?browsers=last 2 version!sass-loader') },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
