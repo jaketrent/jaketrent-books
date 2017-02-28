cc4ce109d793fcbd95540ae9dac12d9e31381fe3
jaketrent <trent.jake@gmail.com>
2017-02-27 20:25:19 -0700

    redirect all traffic to jaketrent.com/book

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,9 @@
 var books = require('../books/books-controller')
 
 exports.map = function (app) {
-  app.get('/', books.index)
-  app.get('/books/:id', books.detail)
+  app.get('*', function (req, res) {
+    res.status(301).redirect('https://jaketrent.com/book/')
+  })
+  // app.get('/', books.index)
+  // app.get('/books/:id', books.detail)
 }
